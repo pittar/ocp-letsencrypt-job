a690da06039164c1862cf9208d84a25cec727588
Andrew Pitt <apitt@redhat.com>
2020-11-17 12:41:57 -0500

    Update aws.sh

--- scripts/aws.sh
+++ scripts/aws.sh
@@ -65,18 +65,17 @@ fi
 echo "Run: ./acme.sh ${install_args[@]}"
 ./acme.sh "${install_args[@]}"
 
-# Router cert secret name.
-ROUTER_CERTS_NAME="router-certs-$(date '+%Y-%m-%d')"
+# Lets Enctrypt cert secret name.
+LE_CERTS_SECRET_NAME="le-certs-$(date '+%Y-%m-%d')"
 
 if [ -f "$FINAL_CERTS/fullchain.pem" ]; then
     secret_args=(
         create
         secret
         tls
-        "$ROUTER_CERTS_NAME"
+        "$LE_CERTS_SECRET_NAME"
         --cert="$FINAL_CERTS/fullchain.pem"
         --key="$FINAL_CERTS/key.pem"
-        -n openshift-ingress
     )
     if [ "$STAGING" == true ] ; then
         secret_args+=(--dry-run=true)
@@ -84,12 +83,18 @@ if [ -f "$FINAL_CERTS/fullchain.pem" ]; then
     fi
 
     # Create tls secret.  Only dry-run and ouptut yaml if STAGING.
-    oc "${secret_args[@]}"
+    oc "${secret_args[@]}" -n openshift-ingress
+    oc "${secret_args[@]}" -n openshift-config
 
     # Patch ingress with new secret if NOT STAGING.
     if [ "$STAGING" == false ] ; then
-        oc patch ingresscontroller default -n openshift-ingress-operator --type=merge --patch='{"spec": { "defaultCertificate": { "name": "'$ROUTER_CERTS_NAME'" }}}'
+        oc patch ingresscontroller default -n openshift-ingress-operator --type=merge --patch='{"spec": { "defaultCertificate": { "name": "'$LE_CERTS_SECRET_NAME'" }}}'
+        # Patch api server with new secret if NOT STAGING.
+        if [ "$PATCH_API_SERVER" == true ] ; then
+            oc patch apiserver cluster --type=merge -p '{"spec":{"servingCerts": {"namedCertificates": [{"names": ["'$LE_API'"], "servingCertificate": {"name": "'$LE_CERTS_SECRET_NAME'"}}]}}}'
+        fi
     fi
+
 else 
     echo "Error generating certs.  Please see logs."
 fi
